db41323d1bd251bebe0192d9bb1ad823673df10a
Paul M <pmolinaup@gmail.com>
2019-05-09 12:00:12 +0530

    added username sorting

--- routes/index.js
+++ routes/index.js
@@ -38,8 +38,9 @@ router.post('/adduser', function(req, res) {
 
 //Get all users
 router.get('/getusers', (req,res) => {
-  User.find({})
+  User.find({}, { username: 1, photo: 1}).collation({ locale: 'en'}).sort({ username: 1 })
   .then(response => {
+    console.log(response)
     res.send({ users: response });
   })
 })
